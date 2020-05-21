4a7b316fd3b6981a6a23b5335dd03e95c7819612
agus_setiadi <a.setiadi@komunigrafik.com>
2020-05-21 11:32:24 +0700

    chart integration vue js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js(['resources/js/app.js', 'resources/js/transaksi.js'], 'public/js')
+mix.js(['resources/js/app.js', 'resources/js/transaksi.js', 'resources/js/dashboard.js'], 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
